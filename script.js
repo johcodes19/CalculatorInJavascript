21f3201101fa8640f5f47c86a587afebcaee5bbe
johCodes <johcodes19@gmail.com>
2023-09-26 11:19:20 +0300

    Added javascript code to do the mathematical expressions using eval() function together with try and catch methods . I also made sure that the inner html of the  div buttons and the display work dynamically.

--- script.js
+++ script.js
@@ -1,5 +1,30 @@
 const buttons= document.querySelectorAll('.button')
+const display= document.querySelector('.display')
 
 buttons.forEach(button=>{
     button.innerHTML = button.dataset.button
+    button.addEventListener('click',()=>{
+        const validInputs = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '*', '/', '%'];
+        
+        if (validInputs.includes(button.innerHTML)) {
+            display.innerHTML += button.dataset.button;
+
+        }else if(button.innerHTML== "="){
+            result()
+        }else if (button.innerHTML=="C"){
+            display.innerHTML=""
+        }
+        
+    })
+
 })
+
+function result(){
+    let operations = display.innerHTML;
+    try {
+        let result = eval(operations);
+        display.innerHTML = result;
+    } catch(e) {
+        display.innerHTML = "Error";
+    }
+}
